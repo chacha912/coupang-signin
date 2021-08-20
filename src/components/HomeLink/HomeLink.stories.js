9a2af75067a73b072d6b577071e28fe1e62dee2f
yeongjongkim <tohero21@naver.com>
2021-08-20 19:11:19 +0900

    feat: change children argument
    1. bind 함수의 arguments 방식에서 스토리북 args 프로퍼티 할당 방식으로 변경
    2. children prop은 args 테이블에 포함되지 않도록 설정

--- src/components/HomeLink/HomeLink.stories.js
+++ src/components/HomeLink/HomeLink.stories.js
@@ -6,23 +6,29 @@ export default {
   component: HomeLink,
   argTypes: {
     as: {
-      options: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
+      // options: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
       control: { type: 'radio' }
     },
     lang: {
-      options: ['en', 'ko'],
-      control: { type: 'radio' }
+      // options: ['en', 'ko'],
+      // control: { type: 'radio' }
+    },
+    children: {
+      table: {
+        disable: true
+      },
+      // control: false
     }
   }
 }
 
 const Template = args => <HomeLink {...args} />;
 
-export const ImageHomeLink = Template.bind({}, { children:<Logo/> });
+export const ImageHomeLink = Template.bind({});
 ImageHomeLink.storyName = '홈 링크 (이미지)';
 ImageHomeLink.args = {
   as: 'h1',
+  children: <Logo/>
 }
 
 export const textHomeLink = Template.bind({});
-textHomeLink.storyName = '홈 링크 (텍스트)';
