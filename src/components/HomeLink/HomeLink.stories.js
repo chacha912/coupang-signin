5f3b7af262aa1a015dd530cf9ef46b47ca8e7119
chacha912 <dkwkdnwk912@gmail.com>
2021-08-20 20:11:40 +0900

    feat: Add href props and textHomeLink story

--- src/components/HomeLink/HomeLink.stories.js
+++ src/components/HomeLink/HomeLink.stories.js
@@ -1,5 +1,5 @@
-import Logo from "components/Logo/Logo";
-import HomeLink from "./HomeLink";
+import Logo from 'components/Logo/Logo';
+import HomeLink from './HomeLink';
 
 export default {
   title: 'Components/UI/HomeLink',
@@ -7,28 +7,39 @@ export default {
   argTypes: {
     as: {
       // options: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
-      control: { type: 'radio' }
+      control: { type: 'radio' },
     },
     lang: {
       // options: ['en', 'ko'],
       // control: { type: 'radio' }
     },
-    children: {
+    href: {
       table: {
-        disable: true
+        disable: true,
       },
-      // control: false
-    }
-  }
-}
+    },
+  },
+};
 
-const Template = args => <HomeLink {...args} />;
+const Template = (args) => <HomeLink {...args} />;
 
 export const ImageHomeLink = Template.bind({});
 ImageHomeLink.storyName = '홈 링크 (이미지)';
 ImageHomeLink.args = {
   as: 'h1',
-  children: <Logo/>
-}
+  children: <Logo />,
+};
+ImageHomeLink.argTypes = {
+  children: {
+    table: {
+      disable: true,
+    },
+  },
+};
 
 export const textHomeLink = Template.bind({});
+textHomeLink.storyName = '홈 링크 (텍스트)';
+textHomeLink.args = {
+  as: 'h1',
+  children: '쿠팡',
+};
