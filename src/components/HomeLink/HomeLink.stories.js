5317c2f12efa3672dc7921532083a40599d4df6b
yeongjongkim <tohero21@naver.com>
2021-08-20 18:57:01 +0900

    feat: add argTypes for control pannel in storybook

--- src/components/HomeLink/HomeLink.stories.js
+++ src/components/HomeLink/HomeLink.stories.js
@@ -4,9 +4,25 @@ import HomeLink from "./HomeLink";
 export default {
   title: 'Components/UI/HomeLink',
   component: HomeLink,
+  argTypes: {
+    as: {
+      options: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
+      control: { type: 'radio' }
+    },
+    lang: {
+      options: ['en', 'ko'],
+      control: { type: 'radio' }
+    }
+  }
 }
 
 const Template = args => <HomeLink {...args} />;
 
 export const ImageHomeLink = Template.bind({}, { children:<Logo/> });
 ImageHomeLink.storyName = '홈 링크 (이미지)';
+ImageHomeLink.args = {
+  as: 'h1',
+}
+
+export const textHomeLink = Template.bind({});
+textHomeLink.storyName = '홈 링크 (텍스트)';
