e541630638cf637e7a0f823fd1f7d16cc0c9ce6a
yeongjongkim <tohero21@naver.com>
2021-08-22 23:33:27 +0900

    refactor: change LoginForm storybook design add on
    LoginForm의 각 story마다 설정되어 있던 design parameter를
    meta에 추가하여 중복 코드 제거

--- src/containers/LoginForm/LoginFrom.stories.js
+++ src/containers/LoginForm/LoginFrom.stories.js
@@ -13,6 +13,13 @@ export default {
   },
   args : {
     width: 576
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url:
+        'https://www.figma.com/file/QNttUlYVQ1wMHo2E2ndD7J/SignIn-%E2%86%90-Coupang?node-id=886%3A37',
+    }
   }
 }
 
@@ -20,13 +27,6 @@ const template = args => <LoginForm {...args}/>
 
 export const mobileLoginForm = template.bind({}); 
 mobileLoginForm.storyName = '로그인 폼 (모바일)';
-mobileLoginForm.parameters = {
-  design: {
-    type: 'figma',
-    url:
-      'https://www.figma.com/file/QNttUlYVQ1wMHo2E2ndD7J/SignIn-%E2%86%90-Coupang?node-id=886%3A37',
-  }
-}
 mobileLoginForm.argTypes = {
   width: { 
     control: { type: 'range', min: 320, max: 800 }
@@ -38,13 +38,6 @@ mobileLoginForm.args = {
 
 export const defaultLoginForm = template.bind({}); 
 defaultLoginForm.storyName = '로그인 폼 (초기 상태)';
-defaultLoginForm.parameters = {
-  design: {
-    type: 'figma',
-    url:
-      'https://www.figma.com/file/QNttUlYVQ1wMHo2E2ndD7J/SignIn-%E2%86%90-Coupang?node-id=886%3A37',
-  }
-}
 defaultLoginForm.argTypes = {
   width: { 
     control: { type: 'range', min: 400, max: 800 }
